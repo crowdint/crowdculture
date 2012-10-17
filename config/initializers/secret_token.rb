1bfdbe3bcb41d6bdee871ae72c3bea31b6fecf54
Ivan Calderon <iwano@hotmail.com>
2012-10-17 13:08:16 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdCulture::Application.config.secret_token = '0fff1e03759db4eeefe0f5500708c814f6eebe2abb729fbceb88cc7dec540ba53d60903d28d1f3a6fcf386490b5c62f7c8553cff03dfee3928384784735c9a9a'
