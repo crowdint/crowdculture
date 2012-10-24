7353d25d734e9d595011f268f7522ac64740b6ce
Ivan Calderon <iwano@hotmail.com>
2012-10-24 13:00:47 -0500

    Save current entries on production's db finished

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,47 +7,5 @@ class StaticPagesController < ApplicationController
       format.js
     end
   end
-
-  def update
-    require 'feedzirra'
-    feeds_urls = Feed.all
-    feeds_urls_array=[]
-    feeds_urls.each do |feed|
-      feeds_urls_array<<feed.url
-    end
-    feeds = Feedzirra::Feed.fetch_and_parse(feeds_urls_array)
-    feeds.keys.each do |url|
-      entries = sort_entries(feeds[url].entries)
-      entries.each do |entry|
-        img =  get_img_src(entry.summary)
-        if !img.blank?
-          author = get_author(entry.entry_id)
-          Entry.create(feed_id: author, img_url: img.to_s, published_date: entry.published, title: entry.title, entry_id:entry.entry_id)
-        end
-      end
-    end
-      @entries = Entry.count
-  end
-
-    private
-
-      def get_img_src(entry)
-        html = Nokogiri::HTML(entry)
-        img = html.css('img/@src')
-      end
-
-      def sort_entries(entries)
-        entries_sorted = entries.sort_by { |i| i.published}.reverse
-      end
-
-      def get_author(entry)
-        if entry.index('culture') != nil
-          author = 1
-        elsif entry.index('amazonaws') != nil
-          author = 3
-        else 
-          author = 2
-        end
-        author
-      end
+  
 end
