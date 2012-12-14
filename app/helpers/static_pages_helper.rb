48ff92e9d25d612317174ed0c5071e1e330cb5cc
Ivan Calderon <iwano@hotmail.com>
2012-12-14 11:12:06 -0600

    Fix links and videos random box color

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -4,7 +4,7 @@ module StaticPagesHelper
   end
 
   def colors
-    %w(#0075C8, #8FC754, #007F73, #7BC2C2, #DBDBBF,"#EEB500, #D64826, #D06B19, #402716)
+    %w(#0075C8 #8FC754 #007F73 #7BC2C2 #DBDBBF #EEB500 #D64826 #D06B19 #402716)
   end
 
   def random_number_from_colors
